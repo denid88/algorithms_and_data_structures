ae8c20b2472de8c50908652e1744b808c771df5c
denid88 <denisdubov88@gmail.com>
2023-07-23 15:45:58 +0300

    [DataStructure] - Stack

---
+++ lib/src/data_structures/stack.dart
@@ -0,0 +1,37 @@
+/// Task: reversed string
+/// input 'Hello World'
+/// output 'dlroW olleH'
+
+void main() {
+  final String input = 'Hello World';
+  final Stack<String> stack = Stack.of(input.split(''));
+  while(stack.isNotEmpty) {
+    print(stack.peek);
+    stack.pop();
+  }
+}
+
+/// Stack implementation
+class Stack<E> {
+  Stack() : _storage = <E>[];
+  final List<E> _storage;
+
+  E get peek => _storage.last;
+
+  bool get isEmpty => _storage.isEmpty;
+
+  bool get isNotEmpty => _storage.isNotEmpty;
+
+  Stack.of(Iterable<E> elements) : _storage = List<E>.of(elements);
+
+  void push(E value) => _storage.add(value);
+
+  E pop() => _storage.removeLast();
+
+  @override
+  String toString() {
+    return '--- Top ---\n'
+      '${_storage.reversed.join('\n')}'
+      '\n-----------';
+  }
+}
